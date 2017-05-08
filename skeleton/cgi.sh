e28f1db66f1e1f85e0aa336cb8731893ca4fb9a7
metalx1000 <git@filmsbykris.com>
2017-05-08 17:02:31 -0400

    fixed variables in cgi

--- skeleton/cgi.sh
+++ skeleton/cgi.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
-echo "Content-type: text/html\n"
-#echo "Environment variables"
-#env
-read QUERY_STRING
+#!/bin/bash
+echo "Content-type: text/plain\n\n"
+echo "Form variables :"
+echo ""
 eval $(echo "$QUERY_STRING"|awk -F'&' '{for(i=1;i<=NF;i++){print $i}}')
-id=`busybox httpd -d $id`
-echo "id=$id"
-name=`busybox httpd -d $name`
-echo "name=$name"
+tmp=`busybox httpd -d $id`
+echo "Text_Field=$tmp"
+tmp=`busybox httpd -d $name`
+echo "Radio_Button=$tmp"
+echo ""
+echo ""
