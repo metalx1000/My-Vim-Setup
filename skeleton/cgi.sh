667aac4989945f452d9dc4b5654abb3e9acff5de
metalx1000 <git@filmsbykris.com>
2017-05-08 17:03:44 -0400

    named variables better

--- skeleton/cgi.sh
+++ skeleton/cgi.sh
@@ -3,9 +3,9 @@ echo "Content-type: text/plain\n\n"
 echo "Form variables :"
 echo ""
 eval $(echo "$QUERY_STRING"|awk -F'&' '{for(i=1;i<=NF;i++){print $i}}')
-tmp=`busybox httpd -d $id`
-echo "Text_Field=$tmp"
-tmp=`busybox httpd -d $name`
-echo "Radio_Button=$tmp"
+id=`busybox httpd -d $id`
+echo "id=$id"
+name=`busybox httpd -d $name`
+echo "name=$name"
 echo ""
 echo ""
