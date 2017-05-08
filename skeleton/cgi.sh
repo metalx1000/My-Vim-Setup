a58bbdb3e7ae76193d379d82e8254c6483708b2c
metalx1000 <git@filmsbykris.com>
2017-05-08 16:47:02 -0400

    added busybox to line for compatiablity

--- skeleton/cgi.sh
+++ skeleton/cgi.sh
@@ -6,5 +6,5 @@ read QUERY_STRING
 eval $(echo "$QUERY_STRING"|awk -F'&' '{for(i=1;i<=NF;i++){print $i}}')
 id=`busybox httpd -d $id`
 echo "id=$id"
-name=`httpd -d $name`
+name=`busybox httpd -d $name`
 echo "name=$name"
