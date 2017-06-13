62c21b3028c99fba1a4c7387bca3f3cdf4e24a01
metalx1000 <git@filmsbykris.com>
2017-06-13 16:33:00 -0400

    changed cube template

--- skeleton/threejs/cube.js
+++ skeleton/threejs/cube.js
@@ -1,15 +1,1 @@
-function createCube(x,y,z){
- if(typeof x === "undefined"){x = 0;}
- if(typeof y === "undefined"){y = 0;}
- if(typeof z === "undefined"){z = 0;}
-
-  var cube = new THREE.Mesh(new THREE.CubeGeometry(200,100,100), new THREE.MeshNormalMaterial());
-  cube.position.x = x;
-  cube.position.y = y;
-  cube.position.z = z;
-  cube.rotation.x = 10;
-  cube.rotation.z = .5;
-  scene.add(cube);
-  return cube;
-}
-
+createCube({x:0,y:0,z:0,sx:1,sy:1,sz:1,material:"normal"});
