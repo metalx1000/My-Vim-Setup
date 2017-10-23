cc4423a6ada0eb6c7a0b6e6a9cd64391bc51b279
metalx1000 <git@filmsbykris.com>
2017-10-23 15:17:31 -0400

    added char string to gcc template

--- skeleton/gcc/chello.c
+++ skeleton/gcc/chello.c
@@ -2,8 +2,9 @@
 #include<stdio.h>
 
 int main(){
+  char msg1[] = "World";
   //printf("Content-type: text/plain\n\n");//for cgi-bin
-  printf("Hello World\n");
+  printf("Hello %s\n",msg1);
   system("ls /");
   return 0;
 }
